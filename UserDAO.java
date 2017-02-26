804c269ab0d0131be0c967c03b4406c747b95ad4
PL <1072336928@qq.com>
2017-02-25 23:14:06 -0600

    Add files via upload

---
+++ UserDAO.java
@@ -0,0 +1,17 @@
+package work01;
+
+/**
+ * 类UserDAO负责用户表的增删盖查操作，它封装了对用户表的全部操作代码
+ * 登录本质上是一个查询用户表的操作
+ * @author PengLiu
+ *
+ */
+public class UserDAO {
+
+	private DBUtil db;
+	
+	public boolean findUser(String userName,String userPassword){
+		
+		return false;
+	}
+}
