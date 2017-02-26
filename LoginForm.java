a559b1c790329aa4ff3787bcdf2c4569a3972da7
PL <1072336928@qq.com>
2017-02-25 23:12:23 -0600

    Add files via upload

---
+++ LoginForm.java
@@ -0,0 +1,24 @@
+package work01;
+
+/**
+ * 类LoginForm负责界面显示：包含与界面有关的方法和事件处理方法
+ * @author PengLiu
+ *
+ */
+public class LoginForm {
+	
+	private UserDAO dao;
+	
+	public void init(){
+		
+	}
+	
+	public void display(){
+		
+	}
+	
+	public void validate(){
+		
+	}
+
+}
