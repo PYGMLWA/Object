44b55d23dc5c0c743b5baee10955d70df5dae32e
PL <1072336928@qq.com>
2017-02-25 23:11:49 -0600

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,14 @@
+package work01;
+/**
+ * 类MainClass负责启动系统，在该类中定义了main()函数。
+ * @author PengLiu
+ *
+ */
+public class MainClass {
+	public static void main(String[] args){
+		
+		//对类LoginForm中的方法进行调用
+		
+	}
+
+}
